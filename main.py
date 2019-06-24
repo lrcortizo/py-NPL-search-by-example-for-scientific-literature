1648f3b3a1cf2df22e11b8b5945fd5f2a5b9268b
Luis Raña <luisraco95@gmail.com>
2019-06-24 21:35:30 +0200

    Comments in QueryByExample script

--- main.py
+++ main.py
@@ -8,41 +8,61 @@ import natural_language_processing
 import text_similarities
 from parameter import Parameter
 
+"""
+Validate input example file
+Input  : input file
+Output : boolean check
+"""
 def valid_file(file):
     if os.path.isfile(file) and file.endswith('.txt'):
         return True
     return False
 
+"""
+Validate given output directory
+Input  : output directory path
+Output : boolean check
+"""
 def valid_directory(dir):
     pattern = re.compile("^[\w\_.@()-]+/?$")
     if pattern.match(dir) is None:
         return False
     return True
 
+"""
+Validate given results number
+Input  : results number
+Output : boolean check
+"""
 def valid_number(num):
     pattern = re.compile(r'\d+')
     if not pattern.match(num) is None and int(num) > 0:
         return True
     return False
 
+"""
+Parse and validate input paramas
+Input  : script params
+Output : parameter object
+"""
 def cli_params(argv):
-    #Initialize input params variables
+    # Initialize input params variables
     search_term = ''
     file = ''
-    output_directory = './' #default
-    max_results = '10' #default
-    verbose = False #default
+    output_directory = './' # default
+    max_results = '10' # default
+    verbose = False # default
 
-    #get params
+    # get params
     try:
         opts, args = getopt.getopt(argv,"hs:f:d:n:v",["help","search=","file=","dir=","number=","verbose"])
     except getopt.GetoptError:
         print ('main.py -s <search_term>  -f <input_file> -d <output_directory> -n <results_number>')
         sys.exit(2)
 
-    #parse and check cli params
+    # parse and check cli params
     for opt, arg in opts:
-        #Help
+        # Help
         if opt in ("-h", "--help"):
             print ('usage: main.py -s <search_term> -f <input_file> -d <output_directory> -n <results_number>\n\
 Options and arguments:\n\
@@ -53,7 +73,7 @@ Options and arguments:\n\
   -v, --verbose: Verbose mode')
             sys.exit()
 
-        #Search term
+        # Search term
         elif opt in ("-s", "--search"):
             if len(arg) > 2:
                 search_term = arg
@@ -61,7 +81,7 @@ Options and arguments:\n\
                 print ('You must introduce a valid search term. At least 3 characters.')
                 sys.exit()
 
-        #Reference file
+        # Example file
         elif opt in ("-f", "--file"):
             if valid_file(arg):
                 file = arg
@@ -69,7 +89,7 @@ Options and arguments:\n\
                 print ('You must introduce a valid input file. Check path and format(txt).')
                 sys.exit()
 
-        #Output directory
+        # Output directory
         elif opt in ("-d", "--dir"):
             if arg.strip() and valid_directory(arg):
                 output_directory = arg
@@ -77,7 +97,7 @@ Options and arguments:\n\
                 print ('You must introduce a valid directory name.')
                 sys.exit()
 
-        #Results number
+        # Results number
         elif opt in ("-n", "--number"):
             if valid_number(arg):
                 max_results = arg
@@ -85,11 +105,11 @@ Options and arguments:\n\
                 print ('You must introduce a valid result number.')
                 sys.exit()
 
-        #Verbose mode
+        # Verbose mode
         elif opt in ("-v", "--verbose"):
             verbose = True
 
-    #check required params
+    # check required params
     if not search_term:
         print ('You must introduce a search term.')
         sys.exit()
@@ -97,32 +117,39 @@ Options and arguments:\n\
         print ('You must introduce a input file.')
         sys.exit()
 
-    #object with the params configuration
+    # object with the params configuration
     parameter = Parameter(search_term, file, output_directory, max_results, verbose)
     return parameter
 
+"""
+Prints info messages when start and finish each step
+Input  : message to print
+"""
 def print_step(msg):
     num = int((100-(len(msg)+2))/2)
     print("\n"+100*"*"+"\n"+num*"-"+" "+msg+" "+num*"-"+"\n"+100*"*"+"\n")
 
-
+"""
+Main class of the application
+Input  : input script parameters
+"""
 if __name__ == "__main__":
     parameter = cli_params(sys.argv[1:])
-
+    # full logging if verbose mode is on
     if parameter.verbose:
         logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
-    #Step 1: Web scraping
+    # Step 1: Web scraping
     print_step("Step 1: Scraping pubmed database")
     extract_data.extract(parameter)
     print_step("End step 1")
 
-    #Step 2: Text processing
+    # Step 2: Text processing
     print_step("Step 2: Natural language processing")
     articles = natural_language_processing.process_docs(parameter)
     print_step("End step 2")
 
-    #Step 3: Text similarities
+    # Step 3: Text similarities
     print_step("Step 3: Text similarities")
     text_similarities.similarity(parameter, articles)
     print_step("End step 3")
