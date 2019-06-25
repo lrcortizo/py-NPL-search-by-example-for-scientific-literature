45e9fdabda2f32af5254f6bd76dfeec7dd933a44
Luis Raña <luisraco95@gmail.com>
2019-06-25 21:31:14 +0200

    switch case simulation

--- QueryByExample.py
+++ QueryByExample.py
@@ -52,119 +52,146 @@ def valid_number(num):
         return True
     return False
 
+######### parsing functions
+# Help
+def parse_help(parameter):
+    print ('usage: python QueryByExample.py -s <search_term> -f <input_file> \n\
+Options and arguments:\n\
+-s, --search: Search term to query pubmed\n\
+-f, --input_file: Input file to compare the results of query\n\
+-d, --dir: Output directory to the output and temporary files\n\
+-n, --results_number: Number of results in pubmed search\n\
+-v, --verbose: Verbose mode\n\
+-p, --procesors: Number of CPU processors\n\
+-t, --topics: Number of topics\n\
+-m, --max_topics: Maximum number of topics\n\
+-c, --coherence_model: Coherence Model mode\n\
+-h, --help: Shows the different options and arguments')
+    sys.exit()
+
+# Search term
+def parse_search_term(arg, parameter):
+    print(arg)
+    if arg.strip():
+        parameter.search_term = arg
+    else:
+        print ('You must introduce a valid search term. At least 3 characters.')
+        sys.exit(1)
+
+def parse_example_file(arg, parameter):
+    if valid_file(arg):
+        parameter.file = arg
+    else:
+        sys.exit(1)
+
+# Output directory
+def parse_output_directory(arg, parameter):
+    if arg.strip() and valid_directory(arg):
+        parameter.output_directory = arg
+    else:
+        print ('You must introduce a valid directory name.')
+        sys.exit(1)
+
+# Results number
+def parse_max_results(arg, parameter):
+    if valid_number(arg):
+        parameter.max_results = arg
+    else:
+        print ('You must introduce a valid result number.')
+        sys.exit(1)
+
+# Coherence model mode
+def parse_verbose(parameter):
+    parameter.verbose = True
+
+# Number of CPU processors
+def parse_processors(arg, parameter):
+    if int(arg) <= multiprocessing.cpu_count():
+        parameter.processors = int(arg)
+    else:
+        print ('Too much processors. This computer has '+str(multiprocessing.cpu_count())+' processors')
+        sys.exit(1)
+
+# Number of topics
+def parse_topics(arg, parameter):
+    if valid_number(arg):
+        parameter.topics = int(arg)
+    else:
+        print ('You must introduce a valid topics number.')
+        sys.exit(1)
+
+# Max topics
+def parse_max_topics(arg, parameter):
+    if valid_number(arg):
+        parameter.max_topics = int(arg)
+    else:
+        print ('You must introduce a valid maximum topics number.')
+        sys.exit(1)
+
+# Coherence model mode
+def parse_coherence_model(parameter):
+    parameter.coherence_model = True
+
 """
 Parse and validate input paramas
 Input  : script params
 Output : parameter object
 """
 def cli_params(argv):
-    # Initialize input params variables
-    search_term = ''
-    file = ''
-    output_directory = './' # default
-    max_results = '10' # default
-    verbose = False # default
-    processors = 1 # default
-    topics =  20 # default
-    max_topics = 200 # default
-    coherence_model = False # default
+    """
+    object with default params configuration
+        search_term = '', file = '', output_directory = './',
+        max_results = '10', verbose = False, processors = 1,
+        topics =  20, max_topics = 200, coherence_model = False,
+    """
+    parameter = Parameter('', '', './', 10, False, 1, 20, 200, False)
+
+    #dict to simulate switch case
+    params = {'-h' : parse_help,
+        '--help' : parse_help,
+        '-s' : parse_search_term,
+        '--search' : parse_search_term,
+        '-f' : parse_example_file,
+        '--file' : parse_example_file,
+        '-d' : parse_output_directory,
+        '--dir' : parse_output_directory,
+        '-n' : parse_max_results,
+        '--number' : parse_max_results,
+        '-v' : parse_verbose,
+        '--verbose' : parse_verbose,
+        '-p' : parse_processors,
+        '--procesors' : parse_processors,
+        '-t' : parse_topics,
+        '--topics' : parse_topics,
+        '-m' : parse_max_topics,
+        '--max_topics' : parse_max_topics,
+        '-c' : parse_coherence_model,
+        '--coherence_model'  : parse_coherence_model,
+    }
 
     # get params
     try:
         opts, args = getopt.getopt(argv,"hs:f:d:n:vp:t:m:c",["help","search=","file=","dir=",
-            "number=","verbose","processors","topics","max_topics","coherence_model"])
+            "number=","verbose","processors=","topics=","max_topics=","coherence_model"])
     except getopt.GetoptError:
         print ('python QueryByExample.py -s <search_term> -f <input_file>')
         sys.exit(2)
 
     # parse and check cli params
     for opt, arg in opts:
-        # Help
-        if opt in ("-h", "--help"):
-            print ('usage: python QueryByExample.py -s <search_term> -f <input_file> \n\
-Options and arguments:\n\
-  -s, --search: Search term to query pubmed\n\
-  -f, --input_file: Input file to compare the results of query\n\
-  -d, --dir: Output directory to the output and temporary files\n\
-  -n, --results_number: Number of results in pubmed search\n\
-  -v, --verbose: Verbose mode\n\
-  -p, --procesors: Number of CPU processors\n\
-  -t, --topics: Number of topics\n\
-  -m, --max_topics: Maximum number of topics\n\
-  -c, --coherence_model: Coherence Model mode\n\
-  -h, --help: Shows the different options and arguments')
-            sys.exit()
-
-        # Search term
-        elif opt in ("-s", "--search"):
-            if arg.strip():
-                search_term = arg
-            else:
-                print ('You must introduce a valid search term. At least 3 characters.')
-                sys.exit(1)
-
-        # Example file
-        elif opt in ("-f", "--file"):
-            if valid_file(arg):
-                file = arg
-            else:
-                sys.exit(1)
-
-        # Output directory
-        elif opt in ("-d", "--dir"):
-            if arg.strip() and valid_directory(arg):
-                output_directory = arg
-            else:
-                print ('You must introduce a valid directory name.')
-                sys.exit(1)
-
-        # Results number
-        elif opt in ("-n", "--number"):
-            if valid_number(arg):
-                max_results = arg
-            else:
-                print ('You must introduce a valid result number.')
-                sys.exit(1)
-
-        # Number of CPU processors
-        elif opt in ("-p", "--procesors"):
-            if int(arg) <= multiprocessing.cpu_count():
-                processors = int(arg)
-            else:
-                print ('Too much processors. This computer has '+str(multiprocessing.cpu_count())+' processors')
-                sys.exit(1)
-
-        # Number of topics
-        elif opt in ("-t", "--topics"):
-            if valid_number(arg):
-                topics = int(arg)
-            else:
-                print ('You must introduce a valid topics number.')
-                sys.exit(1)
-
-        # Max topics
-        elif opt in ("-m", "--max_topics"):
-            if valid_number(arg):
-                max_topics = int(arg)
-            else:
-                print ('You must introduce a valid maximum topics number.')
-                sys.exit(1)
-
-        # Coherence model mode
-        elif opt in ("-c", "--coherence_model"):
-            coherence_model = True
+        if arg:
+            params[opt](arg, parameter)
+        else:
+            params[opt](parameter)
 
     # check required params
-    if not search_term:
+    if not parameter.search_term:
         print ('You must introduce a search term.')
         sys.exit(1)
-    elif not file:
+    elif not parameter.file:
         print ('You must introduce a input file.')
         sys.exit(1)
 
-    # object with the params configuration
-    parameter = Parameter(search_term, file, output_directory, max_results, verbose,
-        processors, topics, max_topics, coherence_model)
     return parameter
 
 """
